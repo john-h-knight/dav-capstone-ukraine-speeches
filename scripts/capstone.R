5b6bc60b64afa349cd8e8d57c7c01f9235d81bd5
John Knight <johnhknight89@gmail.com>
2023-05-20 16:00:09 -0500

    Scraped date, title, link for all speeches in 1st month

--- scripts/capstone.R
+++ scripts/capstone.R
@@ -5,102 +5,90 @@
 # https://ladal.edu.au/webcrawling.html#Introduction
 # https://purrr.tidyverse.org/
 
-
-
-
 library(tidyverse)
 library(rvest)
-# library(robotstxt)
 library(polite)
-# library(readtext)
-# library(flextable)
 
 
 
-# define url
-# this url filters dates for 1st month of conflict
-url <- "https://www.president.gov.ua/en/news/speeches?date-from=24-02-2022&date-to=24-03-2022&"
+# create functions to scrape info from a page
+speech_date_info <- function(session) {
+  
+  speech_date_info <- scrape(session) %>%
+    html_elements(".item_stat_headline p") %>%
+    html_text()
+}
 
-# create polite session object
-session <- bow(url)
+speech_title_info <- function(session) {
+  
+  speech_title_info <- scrape(session) %>% 
+    html_elements(".item_stat_headline h3") %>%
+    html_text()
+}
+
+speech_link_info <- function(session) {
+  
+  speech_link_info <- scrape(session) %>% 
+    html_elements(".item_stat_headline a") %>%
+    html_attr("href")
+}
+
+# vector for the number of pages after filtering for the 1st month
+page_numbers <- 1:7
+
+# define base url that pages numbers will be added to
+base_url <- "https://www.president.gov.ua/en/news/speeches?date-from=24-02-2022&date-to=24-03-2022&page="
+
+# combine base and page number urls
+paging_urls <- paste0(base_url, page_numbers)
 
 # inspect
-session
+paging_urls
 
-# extract links from page 1
-speech_links <- scrape(session) %>%
-  html_elements(".item_stat_headline a") %>%
-  html_attr("href")
+# create tibble for functions
+paging_urls_df <- tibble("page" = c(1:7),
+                         "link" = paging_urls
+                         )
 
-# inspect
-speech_links
+# function to scrape data from each link
+speech_1mo_info <- function(link, page) {
+  
+  page <- rlang::enquo(page)
+  ## `bow()` for every URL link
+  session <- bow(link)
+  
+  ## scrape different info
+  speech_date <- speech_date_info(session = session)
+  
+  speech_title <- speech_title_info(session = session)
+  
+  speech_link <- speech_link_info(session = session)
+  
+  ## combine info into a data frame
+  results <- list(speech_date, speech_title, speech_link)
+  col_names <- c("date", "title", "link") 
+  
+  speech_1mo <- results %>% 
+    reduce(cbind) %>% 
+    as_tibble() %>% 
+    set_names(col_names)
+  
+  return(speech_1mo)
+}
 
+# wrap function for easier debugging
+safe_speech_1mo_info <- safely(speech_1mo_info)
 
+# iterate across each page link
+speech_1mo_df_ALL <- map2(.x = paging_urls_df$link, .y = paging_urls_df$page,
+                                 ~ safe_speech_1mo_info(link = .x, page = .y))
 
+# check to see if any failed, list is length 0, no errors
+speech_1mo_df_ALL %>% 
+  map("error") %>% 
+  purrr::discard(~is.null(.))
 
-
-
-
-
-
-
-
-
-
-
-
-
-session <- bow("https://www.president.gov.ua/en/news/bez-mizhnarodnogo-tribunalu-bez-prityagnennya-do-vidpovidaln-82977")
-
-session
-
-date <- scrape(session) %>%
-  html_elements(".article") %>%
-  html_elements(".date") %>%
-  html_text2()
-
-date
-
-
-
-speech <- scrape(session) %>%
-  html_element(".article_content") %>%
-  html_text2()
-
-speech
-
-
-
-result <- tibble(
-  date = date,
-  speech = speech
-)
-
-result
-
-
-
-
-
-
-first <- bow("https://www.president.gov.ua/en/news/zvernennya-prezidenta-ukrayini-73137")
-
-date_first <- scrape(first) %>%
-  html_elements(".article") %>%
-  html_elements(".date") %>%
-  html_text2()
-
-
-
-speech_first <- scrape(first) %>%
-  html_element(".article_content") %>%
-  html_text2()
-
-
-result_first <- tibble(
-  date = date_first,
-  speech = speech_first
-)
-
-result_first
-
+# since there were no errors, combine into one df
+speech_1mo_df <- speech_1mo_df_ALL %>% 
+  map("result") %>% 
+  bind_rows()
