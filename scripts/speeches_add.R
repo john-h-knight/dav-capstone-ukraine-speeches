c562b8852d95517072f788c1ef9a1a78c8f5e3df
John Knight <johnhknight89@gmail.com>
2023-07-03 07:47:28 -0400

    Scraped June 2023 and appended to data.

--- scripts/speeches_add.R
+++ scripts/speeches_add.R
@@ -4,7 +4,7 @@
 # This script has two main functions. The first scrapes the link to each speech
 # off the speech list. The second then uses that list of links to scrape the
 # title, timestamp, and body of each speech.
-# Dates: 14 Feb 2022 - 31 May 2023
+# Dates: 01 June 2023 - 30 June 2023
 
 library(tidyverse)
 library(rvest)
@@ -35,24 +35,24 @@ list_link_info <- function(session_list) {
 }
 
 # vector for the number of pages in the speech list
-page_numbers <- 1:72
+page_numbers_june <- 1:5
 
 # define base url that pages numbers will be added to
-base_url <- "https://www.president.gov.ua/en/news/speeches?date-from=14-02-2022&date-to=31-05-2023&page="
+base_url_june <- "https://www.president.gov.ua/en/news/speeches?date-from=01-06-2023&date-to=30-06-2023&page="
 
 # combine base and page number urls
-paging_urls <- paste0(base_url, page_numbers)
+paging_urls_june <- paste0(base_url_june, page_numbers_june)
 
 # inspect
-paging_urls
+paging_urls_june
 
 # create tibble for functions
-paging_urls_df <- tibble("page" = c(1:72),
-                         "link" = paging_urls
+paging_urls_df_june <- tibble("page" = c(1:5),
+                         "link" = paging_urls_june
                          )
 
 # function to scrape data from each page of the list
-list_info <- function(link, page) {
+list_info_june <- function(link, page) {
   
   page <- rlang::enquo(page)
   ## `bow()` for every URL link
@@ -69,33 +69,33 @@ list_info <- function(link, page) {
   results <- list(list_timestamp, list_title, list_link)
   col_names <- c("timestamp", "title", "link") 
   
-  list <- results %>% 
+  list_june <- results %>% 
     reduce(cbind) %>% 
     as_tibble() %>% 
     set_names(col_names)
   
-  return(list)
+  return(list_june)
 }
 
 # wrap function for easier debugging
-safe_list_info <- safely(list_info)
+safe_list_info_june <- safely(list_info_june)
 
 # iterate across each page of the list
-list_df_ALL <- map(.x = paging_urls_df$link,
-                       ~ safe_list_info(link = .x))
+list_df_ALL_june <- map(.x = paging_urls_df_june$link,
+                       ~ safe_list_info_june(link = .x))
 
 # check to see if any failed
-list_df_ALL %>% 
+list_df_ALL_june %>% 
   map("error") %>% 
   purrr::discard(~is.null(.))
 
 # if no errors (list length of 0), combine into one df
-list_df <- list_df_ALL %>% 
+list_df_june <- list_df_ALL_june %>% 
   map("result") %>% 
   bind_rows()
 
 # inspect
-list_df
+list_df_june
 
 
 
@@ -125,10 +125,10 @@ page_body_info <- function(session_page) {
 }
 
 # function to scrape data from each speech page
-page_info <- function(link_page) {
+page_info_june <- function(link_page_june) {
   
   ## `bow()` for every URL link
-  session_page <- bow(link_page)
+  session_page <- bow(link_page_june)
   
   ## scrape different info
   page_timestamp <- page_timestamp_info(session_page = session_page)
@@ -141,36 +141,36 @@ page_info <- function(link_page) {
   results_page <- list(page_timestamp, page_title, page_body)
   col_names_page <- c("timestamp", "title", "speech") 
   
-  page <- results_page %>% 
+  page_june <- results_page %>% 
     reduce(cbind) %>% 
     as_tibble() %>% 
     set_names(col_names_page)
   
-  return(page)
+  return(page_june)
 }
 
 # wrap function for easier debugging
-safe_page_info <- safely(page_info)
+safe_page_info_june <- safely(page_info_june)
 
 # iterate across each link
-page_df_ALL <- map(.x = list_df$link,
-                       ~ safe_page_info(link_page = .x))
+page_df_ALL_june <- map(.x = list_df_june$link,
+                       ~ safe_page_info_june(link_page_june = .x))
 
 # check to see if any failed
-page_df_ALL %>% 
+page_df_ALL_june %>% 
   map("error") %>% 
   purrr::discard(~is.null(.))
 
 # if no errors (list length of 0), combine into one df
-page_df <- page_df_ALL %>% 
+page_df_june <- page_df_ALL_june %>% 
   map("result") %>% 
   bind_rows()
 
 # inspect
-page_df
+page_df_june
 
 # export
-write_csv(page_df, file = 'data/speeches_feb_22_to_may_23.csv')
+write_csv(page_df_june, file = 'data/speeches_june_2023.csv')
 
 
 
--- scripts/themes.R
+++ scripts/themes.R
@@ -18,18 +18,114 @@ library(flextable)
 library(reshape2)
 library(textdata)
 
+# reminder ----------------------------------------------------------------
+
 data # raw
 data_words # tokenized
 data_words_cleaned # stopwords removed
 data_words_count # word count per speech
 
+# create, format, arrange -------------------------------------------------
 
+# save as new object
+data_june <- page_df_june
 
-# overview ----------------------------------------------------------------
+# split timestamp into separate date and time columns
+data_june[c('date', 'time')] <- str_split_fixed(data_june$timestamp, " - ", 2)
 
-# plot of word count per speech
-data_words_count %>%
-  ggplot(aes(x = date, y = n)) +
+# drop timestamp and rearrange columns
+data_june <- data_june %>%
+  select(!timestamp) %>%
+  relocate(date, time, .before = title)
+
+# parse date
+data_june$date <- dmy(data_june$date)
+
+# parse time
+data_june$time <- hm(data_june$time)
+
+# arrange by date
+data_june <- data_june %>%
+  arrange(date, time)
+
+# inspect
+str(data_june)
+
+# add id number, represents speech number
+data_june <- data_june %>%
+  mutate(id = 709:753) %>%
+  relocate(id, .before = date)
+
+# add id_day number, represents speech number for a given day
+data_june <- data_june %>%
+  group_by(date) %>%
+  mutate(id_day = 1:n()) %>%
+  relocate(id_day, .after = date) %>%
+  ungroup()
+
+# combine data and data_june to create full df
+data <- bind_rows(data, data_june)
+
+# tidytext prep -----------------------------------------------------------
+
+# unnest single words from speech and place in own row
+data_words <- data %>%
+  select(id, date, id_day, speech) %>%
+  unnest_tokens(output = word,
+                input = speech,
+                token = "words",
+                to_lower = TRUE,
+                drop = TRUE
+  )
+
+# remove stop words
+data_words_cleaned <- data_words %>%
+  anti_join(get_stopwords())
+
+# quanteda prep -----------------------------------------------------------
+
+# create a corpusC
+data_corpus <- data %>%
+  select(id, date, id_day, speech) %>%
+  corpus(text_field = "speech")
+
+# verify docvars
+docvars(data_corpus)
+
+# convert corpus to tibble for joining the docvars with the KWIC tibbles
+data_kwic_join <- data_corpus %>%
+  convert(to = "data.frame") %>%
+  as_tibble() %>%
+  select(!text)
+
+# create tokens from corpus
+data_tokens <- tokens(data_corpus, remove_punct = TRUE) %>%
+  tokens_tolower()
+
+# remove stopwords
+data_tokens_nostop <- tokens_remove(data_tokens, pattern = stopwords("en"))
+
+# speech word counts ------------------------------------------------------
+
+# word count per speech
+data_words_count <- data_words %>%
+  group_by(id) %>%
+  count()
+
+# create df of id, date, id_day for joining
+data_id <- data %>%
+  select(id, date, id_day, time)
+
+# join and rearrange
+data_words_count <- data_words_count %>%
+  full_join(data_id, by = c("id" = "id")) %>%
+  relocate(n, .after = time)
+
+# export for other viz tools
+write_csv(data_words_count, file = 'data/words_count.csv')
+
+# bar plot of word count by speech
+ggplot(data_words_count, aes(x = date, y = n)) +
   geom_col(position = position_dodge2(preserve = "single"), 
            show.legend = FALSE, fill = "black") +
   scale_x_date("date",
@@ -40,13 +136,18 @@ data_words_count %>%
                      minor_breaks = NULL) +
   theme_minimal()
 
+# scatter plot of word count by speech
+ggplot(data_words_count, aes(x = date, y = n)) +
+  geom_point() +
+  theme_minimal()
+
 # frequency ---------------------------------------------------------------
 
 # most common words
 data_words_cleaned %>%
   count(word, sort = TRUE)
 
-# plot most common words
+# plot top 10 most common words
 data_words_cleaned %>%
   count(word, sort = TRUE) %>%
   slice(1:10) %>%
@@ -54,13 +155,7 @@ data_words_cleaned %>%
   geom_col() +
   theme_minimal()
 
-# plot most common words
-data_words_cleaned %>%
-  count(word, sort = TRUE) %>%
-  ggplot(aes(x = reorder(word, -n), y = n)) +
-  geom_col() +
-  theme_minimal() +
-  coord_flip()
+
 
 # words used at least 100 times
 data_words_100x <- data_words_cleaned %>%
